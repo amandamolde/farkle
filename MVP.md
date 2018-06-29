063f003c9f79d54963e2d275f333f33404249db3
Amanda Molde <amanda.molde@gmail.com>
2018-06-29 11:40:12 -0600

    Initialized file structure and added MVP.md file

---
+++ MVP.md
@@ -0,0 +1,33 @@
+# Farkle MVP
+
+turn - player begins with set of 6 fresh dice
+roll - player begins with whatever dice they have left
+
+MVP:
+1. Player 1 and Player 2 displayed as text on screen
+2. Switch between players each turn
+3. Highlight which player's turn it is
+4. Roll dice function for 6 dice (Math.random)
+5. Click on a die to select it for hand
+6. Re-click on a die to de-select it for hand
+7. "Next Roll" button - player ends current roll and moves score to tempScore
+8. Check if out of dice (allow hot hands? nice to have?)
+9. Check for farkle
+10. Remove hand die from dice available to roll next roll
+11. Evaluate hand for score
+12. Display handScore, tempScore, realScore
+13. Store tempScore at end of each roll
+14. Button for banking tempScore to realScore
+15. Check for farkle each roll
+16. Check for winner at end of each turn - other player then gets one last turn
+
+NICE TO HAVES:
+1. Enter your name
+2. Rolling dice animation
+3. Dice roll to determine who goes first
+4. Hot Dice function -- player can re-roll dice if they are able to bank points on all 6 dice
+5. "High-stakes" -- player can choose to begin their turn either with a fresh set of six dices or by throwing the remianing dice after the previous player has completed their turn
+6. Scoring threshold in opening turn or all turns -- player is required to get a certain number of points before they can bank their points
+7. Spicy farkle
+8. Three.js for cube animation
+9. Firebase for mobile
---
+++ js/farkleApp.js
@@ -0,0 +1,39 @@
+// turn - player begins with set of 6 fresh dice
+// roll - player begins with whatever dice they have left
+
+
+
+
+// MVP
+// Player 1 and Player 2 displayed as text on screen
+// Switch between players each turn
+// Highlight which player's turn it is
+// Roll dice function for 6 dice (Math.random)
+// Click on a die to select it for hand
+// Re-click on a die to de-select it for hand
+// "Next Roll" button - player ends current roll and moves score to tempScore
+// Check if out of dice (allow hot hands? nice to have?)
+// Check for farkle
+// Remove hand die from dice available to roll next roll
+// Evaluate hand for score
+// Display handScore, tempScore, realScore
+// Store tempScore at end of each roll
+// Button for banking tempScore to realScore
+// Check for farkle each roll
+// Check for winner at end of each turn - other player then gets one last turn
+
+
+
+
+
+
+// NICE TO HAVES
+// Enter your name
+// Rolling dice animation
+// Dice roll to determine who goes first
+// Hot Dice function -- player can re-roll dice if they are able to bank points on all 6 dice
+// "High-stakes" -- player can choose to begin their turn either with a fresh set of six dices or by throwing the remianing dice after the previous player has completed their turn
+// Scoring threshold in opening turn or all turns -- player is required to get a certain number of points before they can bank their points
+// Spicy farkle
+// Three.js for cube animation
+// Firebase for mobile
