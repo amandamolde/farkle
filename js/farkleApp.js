4bd07f0533c6472262f9b8aa984077379058839e
Amanda Molde <amanda.molde@gmail.com>
2018-07-01 18:51:05 -0600

    cleaned up code and removed console.logs

--- js/farkleApp.js
+++ js/farkleApp.js
@@ -20,67 +20,35 @@ const rollDice = () => {
 };
 
 const displayRoll = () => {
-	// console.log(roll);
 	for (let i = 0; i < roll.length; i++) {
 		let dieValue = roll[i];
-		// console.log(dieValue);
 		const die = $(`<div class="die rollDie">${dieValue}</div>`);
 		$(die).attr("value", `${dieValue}`);
-		// $('.roll').empty();
 		$('.roll').append(die);
 	};
 };
 
-const addDieToHand = () => {
+const addOrRemoveDieFromHand = () => {
 	$('.rollDie').on('click', (e) => {
-		console.log(e.currentTarget);
-		console.log('clicked!')
-		// $(e.currentTarget).css("background-color", "gray");
 		let rollIndex = $(e.currentTarget).index('.rollDie');
-		console.log(rollIndex);
 		let dieRemovedFromRoll = roll.splice(rollIndex, 1)[0];
 		$(e.currentTarget).removeClass('rollDie').addClass('handDie');
 		$(e.currentTarget).detach().appendTo('.currentPlayerHand');
-		console.log(roll + ' is the roll');
-		console.log(dieRemovedFromRoll + ' is the die removed from the roll');
 		currentPlayer.hand.push(dieRemovedFromRoll);
-		console.log(currentPlayer.hand + ' is the playerHand');
+		
 		$('.handDie').off('click').on('click', (e) => {
-			console.log('the hand die has been clicked');
 			let handIndex = $(e.currentTarget).index('.handDie');
-			console.log(handIndex);
 			let dieRemovedFromHand = currentPlayer.hand.splice(handIndex, 1)[0];
 			$(e.currentTarget).removeClass('handDie').addClass('rollDie');
 			$(e.currentTarget).detach().appendTo('.roll');
-			console.log(dieRemovedFromHand + ' is the die removed from the hand');
 			roll.push(dieRemovedFromHand);
-			console.log(roll + ' is the roll');
-			console.log(currentPlayer.hand + ' is the playerHand');
 		});
 	});
 };
 
-// const removeDieFromHand = () => {
-// 	$('.handDie').off('click').on('click', (e) => {
-// 		console.log(e.currentTarget);
-// 		console.log('clicked!')
-// 		// $(e.currentTarget).css("background-color", "gray");
-// 		let handIndex = $(e.currentTarget).index('.handDie');
-// 		console.log(handIndex);
-// 		let dieRemovedFromHand = currentPlayerHand.splice(handIndex, 1)[0];
-// 		$(e.currentTarget).removeClass('handDie').addClass('rollDie');
-// 		$(e.currentTarget).detach().appendTo('.roll');
-// 		console.log(roll + ' is the roll');
-// 		console.log(dieRemovedFromHand + ' is the die removed from the hand');
-// 		roll.push(dieRemovedFromHand);
-// 		console.log(currentPlayer.hand + ' is the playerHand');
-// 	});
-// };
 
 
 
 rollDice();
 displayRoll();
-addDieToHand();
-// removeDieFromHand();
-
+addOrRemoveDieFromHand();
