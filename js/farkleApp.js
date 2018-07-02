0c74bdae5c36762aea026d3e17204f5ebc070bd6
Amanda Molde <amanda.molde@gmail.com>
2018-07-01 20:41:28 -0600

    Added scoring functionality, but still need functionality for  Three Pairs and Two Triplets

--- js/farkleApp.js
+++ js/farkleApp.js
@@ -1,5 +1,12 @@
 let roll = [];
 let currentPlayerHand = [];
+let handScore = 0;
+let countOfOnes = 0;
+let countOfTwos = 0;
+let countOfThrees = 0;
+let countOfFours = 0;
+let countOfFives = 0;
+let countOfSixes = 0;
 
 class Player {
 	constructor (name, hand, handScore, tempScore, totalScore) {
@@ -48,8 +55,105 @@ const addOrRemoveDieFromHand = () => {
 };
 
 
+const countDieInHand = () => {
+	for (let i = 0; i < currentPlayerHand.length; i++) {
+		if (currentPlayerHand[i] == 1) {
+			countOfOnes ++;
+		} else if (currentPlayerHand[i] == 2) {
+			countOfTwos ++;
+		} else if (currentPlayerHand[i] == 3) {
+			countOfThrees ++;
+		} else if (currentPlayerHand[i] == 4) {
+			countOfFours ++;
+		} else if (currentPlayerHand[i] == 5) {
+			countOfFives ++;
+		} else if (currentPlayerHand[i] == 6) {
+			countOfSixes ++;
+		}
+	};
+};
+
+
+const scoreOnes = () => {
+	if (countOfOnes == 1) {
+		handScore += 100;
+	} else if (countOfOnes == 2) {
+		handScore += 200;
+	} else if (countOfOnes == 3) {
+		handScore += 300;
+	};
+};
+
+const scoreTwos = () => {
+	if (countOfTwos == 3) {
+		handScore += 200;
+	};
+};
+
+const scoreThrees = () => {
+	if (countOfThrees == 3) {
+		handScore += 300;
+	};
+};
+
+const scoreFours = () => {
+	if (countOfFours == 3) {
+		handScore += 400;
+	};
+};
+
+const scoreFives = () => {
+	if (countOfFives == 1) {
+		handScore += 50;
+	} else if (countOfFives == 2) {
+		handScore += 100;
+	} else if (countOfFives == 3) {
+		handScore += 500;
+	};
+};
+
+const scoreSixes = () => {
+	if (countOfSixes == 3) {
+		handScore += 600;
+	};
+};
+
+const scoreFourOfAKind = () => {
+	if (countOfOnes == 4 || countOfTwos == 4 || countOfThrees == 4 || countOfFours == 4 || countOfFives == 4 || countOfSixes == 4) {
+		handScore += 1000;
+	};
+};
+
+const scoreFiveOfAKind = () => {
+	if (countOfOnes == 5 || countOfTwos == 5 || countOfThrees == 5 || countOfFours == 5 || countOfFives == 5 || countOfSixes == 5) {
+		handScore += 2000;
+	};
+};
+
+const scoreSixOfAKind = () => {
+	if (countOfOnes == 6 || countOfTwos == 6 || countOfThrees == 6 || countOfFours == 6 || countOfFives == 6 || countOfSixes == 6) {
+		handScore += 3000;
+	};
+};
+
+const scoreStraight = () => {
+	let straight = [];
+	for (let i = 0; i < currentPlayerHand.length; i++) {
+		let x = currentPlayerHand[i];
+
+		if (straight.indexOf(x) === -1) {
+			straight.push(x);
+		};
+	};
+	if (straight.length == 6) {
+		handScore += 1500;
+	};
+};
+
 
 
 rollDice();
 displayRoll();
 addOrRemoveDieFromHand();
+countDieInHand();
+scoreOnes();
